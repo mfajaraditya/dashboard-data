534c5c7b3e20dbe88bfbddade968d826eee5df51
Infamous0192 <infamous0192@gmail.com>
2022-12-27 22:12:22 +0800

    Menambahi migration

--- app/Database/Seeds/InitialSeeder.php
+++ app/Database/Seeds/InitialSeeder.php
@@ -51,38 +51,274 @@ class InitialSeeder extends Seeder
             ],
         ]);
 
-        $this->db->table('')->insertBatch([
+        $this->db->table('indikator')->insertBatch([
             [
                 'nama_indikator' => 'Listrik',
                 'nilai' => 0,
+                'id_kategori' => 1,
             ],
             [
-                'nama_indikator' => 'Gas > 3kg',
+                'nama_indikator' => 'Gas > 3 kg',
                 'nilai' => 0,
+                'id_kategori' => 1,
             ],
             [
-                'nama_indikator' => 'Gas 3kg',
+                'nama_indikator' => 'Gas 3 kg',
                 'nilai' => 0,
+                'id_kategori' => 1,
             ],
             [
                 'nama_indikator' => 'Gas Kota/Biogas',
                 'nilai' => 0,
+                'id_kategori' => 1,
             ],
             [
-                'nama_indikator' => 'Minyak Tanah',
+                'nama_indikator' => 'Minyak tanah',
                 'nilai' => 80,
+                'id_kategori' => 1,
             ],
             [
-                'nama_indikator' => 'Listrik',
+                'nama_indikator' => 'Briket',
                 'nilai' => 0,
+                'id_kategori' => 1,
             ],
             [
-                'nama_indikator' => 'Listrik',
-                'nilai' => 0,
+                'nama_indikator' => 'Arang',
+                'nilai' => 95,
+                'id_kategori' => 1,
             ],
             [
-                'nama_indikator' => 'Listrik',
+                'nama_indikator' => 'Kayu Bakar',
+                'nilai' => 90,
+                'id_kategori' => 1,
+            ],
+            [
+                'nama_indikator' => 'Tidak memasak di rumah',
                 'nilai' => 0,
+                'id_kategori' => 1,
+            ],
+        ]);
+
+        $this->db->table('indikator')->insertBatch([
+            [
+                'nama_indikator' => '450 watt',
+                'nilai' => 70,
+                'id_kategori' => 2,
+            ],
+            [
+                'nama_indikator' => '900 watt',
+                'nilai' => 50,
+                'id_kategori' => 2,
+            ],
+            [
+                'nama_indikator' => '1.300 watt',
+                'nilai' => 0,
+                'id_kategori' => 2,
+            ],
+            [
+                'nama_indikator' => '2.200 watt',
+                'nilai' => 0,
+                'id_kategori' => 2,
+            ],
+            [
+                'nama_indikator' => '> 2.200 watt',
+                'nilai' => 0,
+                'id_kategori' => 2,
+            ],
+            [
+                'nama_indikator' => 'Tanpa meteran',
+                'nilai' => 0,
+                'id_kategori' => 2,
+            ],
+        ]);
+
+        $this->db->table('indikator')->insertBatch([
+            [
+                'nama_indikator' => 'Sendiri',
+                'nilai' => 0,
+                'id_kategori' => 3,
+            ],
+            [
+                'nama_indikator' => 'Bersama',
+                'nilai' => 90,
+                'id_kategori' => 3,
+            ],
+            [
+                'nama_indikator' => 'Umum',
+                'nilai' => 90,
+                'id_kategori' => 3,
+            ],
+            [
+                'nama_indikator' => 'Tidak ada',
+                'nilai' => 100,
+                'id_kategori' => 3,
+            ],
+        ]);
+
+        $this->db->table('indikator')->insertBatch([
+            [
+                'nama_indikator' => 'Beton/genteng beton',
+                'nilai' => 0,
+                'id_kategori' => 4,
+            ],
+            [
+                'nama_indikator' => 'Genteng keramik',
+                'nilai' => 0,
+                'id_kategori' => 4,
+            ],
+            [
+                'nama_indikator' => 'Genteng metal',
+                'nilai' => 0,
+                'id_kategori' => 4,
+            ],
+            [
+                'nama_indikator' => 'Genteng tanah liat',
+                'nilai' => 30,
+                'id_kategori' => 4,
+            ],
+            [
+                'nama_indikator' => 'Asbes',
+                'nilai' => 40,
+                'id_kategori' => 4,
+            ],
+            [
+                'nama_indikator' => 'Seng',
+                'nilai' => 50,
+                'id_kategori' => 4,
+            ],
+            [
+                'nama_indikator' => 'Sirap',
+                'nilai' => 60,
+                'id_kategori' => 4,
+            ],
+            [
+                'nama_indikator' => 'Bambu',
+                'nilai' => 70,
+                'id_kategori' => 4,
+            ],
+            [
+                'nama_indikator' => 'Jerami/ijuk/daun-daunan/rumbia',
+                'nilai' => 80,
+                'id_kategori' => 4,
+            ],
+            [
+                'nama_indikator' => 'Lainnya',
+                'nilai' => 0,
+                'id_kategori' => 4,
+            ],
+        ]);
+
+        $this->db->table('indikator')->insertBatch([
+            [
+                'nama_indikator' => 'Tembok',
+                'nilai' => 0,
+                'id_kategori' => 5,
+            ],
+            [
+                'nama_indikator' => 'Plesteran anyaman bambu/kawat',
+                'nilai' => 40,
+                'id_kategori' => 5,
+            ],
+            [
+                'nama_indikator' => 'Kayu',
+                'nilai' => 30,
+                'id_kategori' => 5,
+            ],
+            [
+                'nama_indikator' => 'Anyaman bambu',
+                'nilai' => 60,
+                'id_kategori' => 5,
+            ],
+            [
+                'nama_indikator' => 'Batang kayu',
+                'nilai' => 90,
+                'id_kategori' => 5,
+            ],
+            [
+                'nama_indikator' => 'Bambu',
+                'nilai' => 70,
+                'id_kategori' => 5,
+            ],
+            [
+                'nama_indikator' => 'Lainnya',
+                'nilai' => 0,
+                'id_kategori' => 5,
+            ],
+        ]);
+
+        $this->db->table('indikator')->insertBatch([
+            [
+                'nama_indikator' => 'Leher angsa',
+                'nilai' => 0,
+                'id_kategori' => 6,
+            ],
+            [
+                'nama_indikator' => 'Plengsengan',
+                'nilai' => 0,
+                'id_kategori' => 6,
+            ],
+            [
+                'nama_indikator' => 'Cemplung/cubluk',
+                'nilai' => 80,
+                'id_kategori' => 6,
+            ],
+            [
+                'nama_indikator' => 'Tidak pakai',
+                'nilai' => 100,
+                'id_kategori' => 6,
+            ],
+        ]);
+
+        $this->db->table('indikator')->insertBatch([
+            [
+                'nama_indikator' => 'Marmer/granit',
+                'nilai' => 0,
+                'id_kategori' => 7,
+            ],
+            [
+                'nama_indikator' => 'Keramik',
+                'nilai' => 0,
+                'id_kategori' => 7,
+            ],
+            [
+                'nama_indikator' => 'Parket/vinil/permadani',
+                'nilai' => 0,
+                'id_kategori' => 7,
+            ],
+            [
+                'nama_indikator' => 'Ubin/tegel/teraso',
+                'nilai' => 0,
+                'id_kategori' => 7,
+            ],
+            [
+                'nama_indikator' => 'Kayu/papan kualitas tinggi',
+                'nilai' => 0,
+                'id_kategori' => 7,
+            ],
+            [
+                'nama_indikator' => 'Semen/batu merah',
+                'nilai' => 40,
+                'id_kategori' => 7,
+            ],
+            [
+                'nama_indikator' => 'Bambu',
+                'nilai' => 90,
+                'id_kategori' => 7,
+            ],
+            [
+                'nama_indikator' => 'Kayu/papan kualitas rendah',
+                'nilai' => 80,
+                'id_kategori' => 7,
+            ],
+            [
+                'nama_indikator' => 'Tanah',
+                'nilai' => 100,
+                'id_kategori' => 7,
+            ],
+            [
+                'nama_indikator' => 'Lainnya',
+                'nilai' => 0,
+                'id_kategori' => 7,
             ],
         ]);
     }
